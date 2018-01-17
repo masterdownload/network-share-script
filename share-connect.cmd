4d627bf06edf10abfa0c9fafd1777b273d9bd6d9
Scott Evensen <scottman741@hotmail.com>
2018-01-16 17:23:53 -0800

    finished bata version commit v0.1

--- share-connect.cmd
+++ share-connect.cmd
@@ -1,12 +1,11 @@
 REM This scipt is to reconnect network drive(s) that Steam apps are located after a reboot in windows client. 
-REM requirements for this script. 1) test.txt at the root of the share location. 2) drive mapped with correct permissions 3)Steam auto start disabled 4) this script configured in a scheduled task
+REM requirements for this script. 1) test.txt at the root of the share location. 2) drive mapped with correct permissions 3)Steam auto start disabled 4) this script configured in a scheduled task to launch at startup
 
 IF EXIST "G:\test.txt" (
     ECHO share found
 	timeout 3
 	cd "%HOMEPATH%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam"
 	start steam
-	exit
 ) ELSE (
     ECHO share not found connecting drive
 	start /min explorer G:
@@ -14,5 +13,4 @@ IF EXIST "G:\test.txt" (
 	taskkill /im explorer.exe
 	cd "%HOMEPATH%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam"
 	start steam
-
 )
